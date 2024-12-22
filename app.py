b0270f541dd0e6ff62865a3768a6e39beccb09a5
KAVERAPPA <114868311+ka25van@users.noreply.github.com>
2024-12-22 22:30:28 +0530

    Added comments
    Detailed Comments for understanding the flow

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import json
 from datetime import datetime
 
 
-def blog_generator_bedrock(message:str):
-    prompt=f"Write a 100 words creative blog on the topic {blogtopic}"
+def topic_generator_bedrock(topic:str):
+    prompt=f"Provide information with proper details for {topic}"
+
+#as per the model that we are using it requires the body
 
     body={
         "prompt": prompt,
@@ -16,51 +18,52 @@ def blog_generator_bedrock(message:str):
         }
     }
 
+#Using boto3 you can use the aws by calling it's client
     try:
         bedrock=boto3.client("bedrock-runtime",region_name="us-east-1",
                              config=botocore.config.Config(read_timeout=300,retries={"max_attempts":3}))
         response = bedrock.invoke_model(body=json.dumps(body),modelId="meta.llama3-2-1b-instruct-v1:0")              
         response_content = response.get('body').read()
         res_data=json.loads(response_content)
-        blog_details=res_data['generation']
-        return blog_details
+        topic_details=res_data['generation']
+        return topic_details
     except Exception as e:
         print(f"Error",{e})
 
-    
 
-def save_to_s3(s3_key,s3_name,blog):
+#whatever the response we get it will be stored in S3 bucket, we can verify this in cloudwatch too.
+
+def save_to_s3(s3_key,s3_name,topic):
     s3=boto3.client('s3')
 
     try:
-        s3.put_object(Bucket = s3_name, Key=s3_key, Body=blog)
+        s3.put_object(Bucket = s3_name, Key=s3_key, Body=topic)
         print("Code saved to s3")
     except Exception as e:
         print(f"Error in s3", {e})
 
 
 
-
+#using lambda function to generate the connection. In the AMC i have added the API gateway to trigger the lambda function
 def lambda_handler(event, context):
-    # TODO implement
     event = json.loads(event['body'])
-    blogtopic = event['blog_topic']
+    topic = event['topic_']
 
-    blog_gen=blog_generator_bedrock(blogtopic=blogtopic)
+    topic_gen=topic_generator_bedrock(topic=topic)
 
-    if blog_gen:
+    if topic_gen:
         current_time=datetime.now().strftime('%H%M%S')
-        s3_bucket_key=f"blog-bucket/{current_time}.txt"
+        s3_bucket_key=f"topic-bucket/{current_time}.txt"
         s3_bucket_name="first_bedrock"
-        save_to_s3(s3_bucket_key,s3_bucket_name, blog_gen)
+        save_to_s3(s3_bucket_key,s3_bucket_name, topic_gen)
 
     else:
-        print("No blogs generated")
+        print("No topics generated")
 
 
     return{
         'status':200,
-        "body": json.dumps("Blog Generated")
+        "body": json.dumps("topic Generated")
     }
 
 
